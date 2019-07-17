220c4a0e3fe84e7d0f39ca8ebeb064069bed45c4
Victoire <victoirebaron@hotmail.fr>
2019-07-17 16:45:24 -0700

    Installation packages

---
+++ server.js
@@ -0,0 +1,9 @@
+// Dependencies
+var express = require("express");
+var mongojs = require("mongojs");
+// Axios and cheerio for the web scraping
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+var app = express();
+
